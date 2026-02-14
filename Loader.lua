ec7988df20209f5cc49f222ec9b026b1480351f4
Potter8899 <136480339+Potter8899@users.noreply.github.com>
2026-02-14 14:31:20 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,25 +4,25 @@ getgenv().ESP_LOADED = true
 local correctKey = "hubpot"
 local url = "https://raw.githubusercontent.com/Potter8899/esp-loader/main/Main.lua"
 
--- UI
-local gui = Instance.new("ScreenGui", game.CoreGui)
-gui.Name = "KeyUI"
+local player = game.Players.LocalPlayer
+local gui = Instance.new("ScreenGui")
+gui.Parent = player:WaitForChild("PlayerGui")
 
 local frame = Instance.new("Frame", gui)
-frame.Size = UDim2.new(0, 260, 0, 140)
-frame.Position = UDim2.new(0.5, -130, 0.5, -70)
+frame.Size = UDim2.new(0,260,0,140)
+frame.Position = UDim2.new(0.5,-130,0.5,-70)
 frame.BackgroundColor3 = Color3.fromRGB(30,30,30)
 
 local title = Instance.new("TextLabel", frame)
 title.Size = UDim2.new(1,0,0,30)
-title.Text = "Enter Key"
+title.Text = "KEY SYSTEM"
 title.TextColor3 = Color3.new(1,1,1)
 title.BackgroundTransparency = 1
 
 local box = Instance.new("TextBox", frame)
 box.Size = UDim2.new(1,-20,0,40)
 box.Position = UDim2.new(0,10,0,40)
-box.PlaceholderText = "ใส่คีย์..."
+box.PlaceholderText = "Enter Key..."
 box.Text = ""
 
 local btn = Instance.new("TextButton", frame)
